14b2d5b72c25c844ec29d7d60bc4c710f579a824
diki <muliaandiki@gmaiil.com>
2025-10-23 21:46:18 +0700

    feat: sidebar for phone

--- src/core/components/app-sidebar.tsx
+++ src/core/components/app-sidebar.tsx
@@ -20,7 +20,6 @@ import {
   useSidebar,
 } from '@/components/ui/sidebar';
 import { MenuDataRestaurant, MenuDataUser } from '@/configs/components.config';
-import { useLogout } from '@/hooks/mutation/auth/mutation';
 import { useAppNameSpase } from '@/hooks/useNameSpace';
 import { cn } from '@/utils/classname';
 import { kebabCaseToWords } from '@/utils/string.format';
@@ -32,11 +31,18 @@ export function AppSidebar() {
 
   const last = pathname.split('/').pop();
   const { currentRole } = useAppNameSpase();
-  const logout = useLogout();
 
   return (
     <Sidebar collapsible="icon" className="border-r">
       <SidebarHeader className="border-b p-4 h-20 flex justify-center">
+        {/* <div className="flex items-center gap-2 md:hidden">
+          <SidebarTrigger className="flex items-center">
+            <Image src="/images/logo.svg" alt="Logo" width={40} height={40} loading="lazy" />
+          </SidebarTrigger>
+          <Label className="text-lg font-semibold truncate max-w-[120px]">
+            {kebabCaseToWords(last!)}
+          </Label>
+        </div> */}
         {isCollapsed ? (
           <SidebarTrigger className="flex justify-center  items-center">
             <Image src="/images/logo.svg" alt="Logo" width={200} height={200} loading="lazy" />
@@ -46,9 +52,7 @@ export function AppSidebar() {
             <SidebarTrigger>
               <Image src="/images/logo.svg" alt="Logo" width={70} height={70} loading="lazy" />
             </SidebarTrigger>
-            <Label className="text-xl text-[var(--label)] font-semibold">
-              {kebabCaseToWords(last!)}
-            </Label>
+            <Label className="text-xl font-semibold">{kebabCaseToWords(last!)}</Label>
           </div>
         )}
       </SidebarHeader>
